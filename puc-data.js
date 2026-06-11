8ecd4b6282e54f4ee960c8339dcf2512a67a59d8
Thiago Medeiros <thiago.medeiros.carvalho@gmail.com>
2026-06-11 11:58:05 -0300

    add checkpoint for puc-si

--- puc-data.js
+++ puc-data.js
@@ -58,7 +58,8 @@ window.portfolioData = {
             "rota": "puc/pi-si.html",
             "recursos": [
                 { "titulo": "Rubrica Sprint 1", "link": "puc_si/rubricas/sprint-1.html", "icone": "fa-clipboard-check" },
-                { "titulo": "Rubrica Sprint 2", "link": "puc_si/rubricas/sprint-2.html", "icone": "fa-clipboard-list" }
+                { "titulo": "Rubrica Sprint 2", "link": "puc_si/rubricas/sprint-2.html", "icone": "fa-clipboard-list" },
+                { "titulo": "Rubrica Sprint 3", "link": "puc_si/rubricas/sprint-3.html", "icone": "fa-flag-checkered" }
             ],
             "cronograma_link": "https://docs.google.com/spreadsheets/d/1Njc9_jsPZ5YPB5FR3fnJ4fekevkRKv7jmdf6YG6Iegc/edit?usp=sharing",
             "cronograma": [
